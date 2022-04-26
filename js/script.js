8518b1c4baa63d9379d378bf8f280ab71481b8a8
Egor <melnikeg@ukr.net>
2022-04-27 01:07:52 +0300

    Created index.html, added null.scss, style.scss and first images

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function ibg() {
+    let ibg = document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+        if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+        }
+    }
+}
+ibg();
+
+$('.icon-menu').click(function(event) {
+    $(this).toggleClass('active');
+    $('.menu__body').toggleClass('active');
+    $('body').toggleClass('lock');
+});
