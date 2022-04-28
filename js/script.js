6d128086d30dd07d6341b95e32d1d53990996cd0
Egor <melnikeg@ukr.net>
2022-04-28 10:31:10 +0300

    Added slider (slick-slider)

--- js/script.js
+++ js/script.js
@@ -13,3 +13,23 @@ $('.icon-menu').click(function(event) {
     $('.menu__body').toggleClass('active');
     $('body').toggleClass('lock');
 });
+
+//SLIDER
+if($('.slider__body').length>0){
+    $('.slider__body').slick({
+        //autoplay: true,
+        //infinite: false,
+        dots: true,
+        arrows: false,
+        accessibility: false,
+        sliderToShow: 1,
+        autoplaySpeed: 3000,
+        adaptiveHeight: true,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {}
+        }]
+    });
+ }
